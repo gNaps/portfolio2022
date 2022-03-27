50911ec1857e4e2b881fc0f3584128f31971b121
gNaps <gabriele.nap@gmail.com>
2022-03-27 16:42:51 +0200

    resume props page

---
+++ frontend/api/models/ResumeProps.ts
@@ -0,0 +1,4 @@
+export interface ResumeProps {
+  jobs: any[];
+  educations: any[];
+}
---
+++ frontend/pages/resume.tsx
@@ -0,0 +1,23 @@
+import type { GetStaticProps, NextPage } from "next";
+import Image from "next/image";
+import { getAboutData } from "../api/aboutService";
+import { AboutProps } from "../api/models/AboutProps";
+import { ResumeProps } from "../api/models/ResumeProps";
+import { getResumeData } from "../api/resumeService";
+import Layout from "../components/Layout/Layout";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const resumeData = await getResumeData();
+  return {
+    props: {
+      ...resumeData,
+    },
+    revalidate: 86400,
+  };
+};
+
+const About: NextPage<ResumeProps> = ({ jobs, educations }) => {
+  return <Layout></Layout>;
+};
+
+export default About;
