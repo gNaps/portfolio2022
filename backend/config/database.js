3f712481d7d5c21adff3f32ed68c8725ecf98b0b
gNaps <gabriele.nap@gmail.com>
2022-04-26 20:56:23 +0200

    upd database structure

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,25 +5,19 @@ module.exports = ({ env }) => {
   if (env("NODE_ENV") === "production") {
     const config = parse(process.env.DATABASE_URL);
     return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-            ssl: {
-              rejectUnauthorized: false,
-            },
-          },
-          options: {
-            ssl: true,
+      connection: {
+        client: "postgres",
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
           },
         },
+        debug: false,
       },
     };
   }
